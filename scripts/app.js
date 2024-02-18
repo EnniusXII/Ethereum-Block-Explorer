5e09753f3f4584afc344ee474c37a6b2982f24c9
Stefan Lahtinen <stefan.lahtinen@gmail.com>
2024-02-18 16:34:44 +0100

    checkBalance and sendTransaction working

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+const accountInput = document.querySelector('#account-number');
+const checkBalanceButton = document.querySelector('#check-balance');
+const displayBalance = document.querySelector('#balance');
+const valueInput = document.querySelector('#amount');
+const toAccountInput = document.querySelector('#to-account');
+const sendButton = document.querySelector('#send-button');
+
+const rpc = new Web3('HTTP://127.0.0.1:7545');
+
+let account;
+
+async function checkBalance() {
+    account = accountInput.value;
+
+    const balance = await rpc.eth.getBalance(account);
+    displayBalance.textContent = `${rpc.utils.fromWei(balance, 'ether')} ETH`
+}
+
+async function sendTransaction() {
+    const amount = valueInput.value;
+    const toAddress = toAccountInput.value;
+
+    try {
+        const send = await rpc.eth.sendTransaction({
+            from: account,
+            to: toAddress,
+            value: rpc.utils.toWei(amount, 'ether'),
+            gas: 21000,
+            gasPrice: 20000000000,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+checkBalanceButton.addEventListener('click', checkBalance)
+sendButton.addEventListener('click', sendTransaction)
